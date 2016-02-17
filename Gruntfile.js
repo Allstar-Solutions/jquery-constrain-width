dc4195ff12e3bd521cb681f381ad0e34c73253ae
John Hume <john@sjconsulting.us>
2016-02-17 16:26:15 +0000

    Initial commit. Plugin working.

---
+++ Gruntfile.js
@@ -0,0 +1,109 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+
+		// Import package manifest
+		pkg: grunt.file.readJSON("package.json"),
+
+		// Banner definitions
+		meta: {
+			banner: "/*\n" +
+				" *  <%= pkg.title || pkg.name %> - v<%= pkg.version %>\n" +
+				" *  <%= pkg.description %>\n" +
+				" *  <%= pkg.homepage %>\n" +
+				" *\n" +
+				" *  Made by <%= pkg.author.name %>\n" +
+				" *  Under <%= pkg.license %> License\n" +
+				" */\n"
+		},
+
+		// Concat definitions
+		concat: {
+			options: {
+				banner: "<%= meta.banner %>"
+			},
+			dist: {
+				src: ["src/jquery.constrainToParentWidth.js"],
+				dest: "dist/jquery.constrainToParentWidth.js"
+			},
+		},
+
+		// Lint definitions
+		jshint: {
+			files: ["src/jquery.constrainToParentWidth.js", "test/**/*"],
+			options: {
+				jshintrc: ".jshintrc"
+			}
+		},
+
+		// Minify definitions
+		uglify: {
+			my_target: {
+				src: ["dist/jquery.constrainToParentWidth.js"],
+				dest: "dist/jquery.constrainToParentWidth.min.js"
+			},
+			options: {
+				banner: "<%= meta.banner %>"
+			}
+		},
+
+		// CSS minify.
+		cssmin: {
+			target: {
+				files: [{
+					expand: true,
+					src: ['*.css', '!*.min.css'],
+					cwd: 'src/css',
+					dest: 'dist/css',
+					ext: '.min.css'
+				}]
+			}
+		},
+
+		// CoffeeScript compilation
+		coffee: {
+			compile: {
+				files: {
+					"dist/jquery.constrainToParentWidth.js": "src/jquery.constrainToParentWidth.coffee"
+				}
+			}
+		},
+
+		// karma test runner
+		karma: {
+		  unit: {
+		    configFile: "karma.conf.js",
+				background: true,
+				singleRun: false,
+				browsers: ["PhantomJS", "Firefox"]
+		  },
+		  //continuous integration mode: run tests once in PhantomJS browser.
+		  travis: {
+		    configFile: "karma.conf.js",
+		    singleRun: true,
+		    browsers: ["PhantomJS"]
+		  },
+		},
+
+		// watch for changes to source
+		// Better than calling grunt a million times
+		// (call 'grunt watch')
+		watch: {
+	    files: ["src/*", "test/**/*", "src/css/*"],
+	    tasks: ["default"]
+		}
+
+	});
+
+	grunt.loadNpmTasks("grunt-contrib-concat");
+	grunt.loadNpmTasks("grunt-contrib-cssmin");
+	grunt.loadNpmTasks("grunt-contrib-jshint");
+	grunt.loadNpmTasks("grunt-contrib-uglify");
+	grunt.loadNpmTasks("grunt-contrib-coffee");
+	grunt.loadNpmTasks("grunt-contrib-watch");
+	grunt.loadNpmTasks("grunt-karma");
+
+	grunt.registerTask("travis", ["jshint", "karma:travis"]);
+	grunt.registerTask("build", ["concat", "uglify"]);
+	grunt.registerTask("default", ["jshint", "build", "karma:unit:run"]);
+};
---
+++ dist/jquery.constrainToParentWidth.js
@@ -0,0 +1,113 @@
+/*
+ *  jquery-boilerplate - v4.0.0
+ *  A jump-start for jQuery plugins development.
+ *  http://jqueryboilerplate.com
+ *
+ *  Made by Zeno Rocha
+ *  Under MIT License
+ */
+// the semi-colon before function invocation is a safety net against concatenated
+// scripts and/or other plugins which may not be closed properly.
+;(function ( $, window, document, undefined ) {
+
+	"use strict";
+
+	// Allow multiple instances
+	var instanceId = 0;
+
+		// Create the defaults once
+		var pluginName = "constrainToParentWidth",
+			defaults = {};
+
+		// The actual plugin constructor
+		function Plugin ( element, options ) {
+				this.element = element;
+				// jQuery has an extend method which merges the contents of two or
+				// more objects, storing the result in the first object. The first object
+				// is generally empty as we don't want to alter the default options for
+				// future instances of the plugin
+				this.settings = $.extend( {}, defaults, options );
+				this._defaults = defaults;
+				this._name = pluginName;
+				this.init();
+		}
+
+		// Avoid Plugin.prototype conflicts
+		$.extend(Plugin.prototype, {
+
+				init: function () {
+						this.constrainToParentWidth();
+						$(window).resize(this.constrainToParentWidth);
+				},
+				constrainToParentWidth: function() {
+					var $this = $(this.element);
+					var $parent = $this.parent();
+
+					if($this.width() > $parent.width()) {
+						$this.width($parent.width());
+					}
+				}
+		});
+
+		// A really lightweight plugin wrapper around the constructor,
+		// allowing multiple instantiations.
+		$.fn.constrainToParentWidth = function ( options ) {
+			var args = arguments;
+
+	        // Is the first parameter an object (options), or was omitted,
+	        // instantiate a new instance of the plugin.
+	        if (options === undefined || typeof options === 'object') {
+	            return this.each(function () {
+
+	                // Only allow the plugin to be instantiated once,
+	                // so we check that the element has no plugin instantiation yet
+	                if (!$.data(this, 'plugin_' + pluginName)) {
+
+	                    // Set the instance Id.
+	                    $.data(this, 'plugin_' + pluginName + '_instanceId', instanceId);
+	                    instanceId++;
+
+	                    // if it has no instance, create a new one,
+	                    // pass options to our plugin constructor,
+	                    // and store the plugin instance
+	                    // in the elements jQuery data object.
+	                    $.data(this, 'plugin_' + pluginName, new Plugin( this, options ));
+	                }
+	            });
+
+	        // If the first parameter is a string and it doesn't start
+	        // with an underscore or "contains" the `init`-function,
+	        // treat this as a call to a public method.
+	        } else if (typeof options === 'string' && options[0] !== '_' && options !== 'init') {
+
+	            // Cache the method call
+	            // to make it possible
+	            // to return a value
+	            var returns;
+
+	            this.each(function () {
+	                var instance = $.data(this, 'plugin_' + pluginName);
+
+	                // Tests that there's already a plugin-instance
+	                // and checks that the requested public method exists
+	                if (instance instanceof Plugin && typeof instance[options] === 'function') {
+
+	                    // Call the method of our plugin instance,
+	                    // and pass it the supplied arguments.
+	                    returns = instance[options].apply( instance, Array.prototype.slice.call( args, 1 ) );
+	                }
+
+	                // Allow instances to be destroyed via the 'destroy' method
+	                if (options === 'destroy') {
+	                  $.data(this, 'plugin_' + pluginName, null);
+	                }
+	            });
+
+	            // If the earlier cached method
+	            // gives a value back return the value,
+	            // otherwise return this to preserve chainability.
+	            return returns !== undefined ? returns : this;
+	        }
+		};
+
+})( jQuery, window, document );
